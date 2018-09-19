cb624f278d776f05eb406946adf6dc46ea41c575
Christy <christy.c051@stu.nebo.edu>
2018-09-19 10:30:16 -0600

    added character and ground

--- 2DGame/Assets/Scripts/CharacterMove.cs
+++ 2DGame/Assets/Scripts/CharacterMove.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
+    // Player Movement Variables
+	public int MoveSpeed;
+	public float JumpHeight;
+
+	// Player grounded variables
+	public Tranform groundcheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround
+	private bool grounded;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+
+	void FixedUpdate () {
+		grounded = Physics2D.OverLapCircle(groundCheck.position, groundCheckRarius, whatIsGround);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
